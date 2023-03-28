100b6dea135be7c47d5d60683bb14cd0765ba804
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-28 09:53:11 -0700

    feat: initial commit to create KMS key in us-east-1 (#1)
    * feat: creating KMS key in us-east-1
    * terraform-docs: automated action
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # terraform-module-cloud-aws-dnssec-kms-key
+<!-- BEGIN_TF_DOCS -->
+
+This Terraform module creates an AWS KMS key in us-east-1 to be used by Route53 DNSSEC.
+
+Note: This is regional to us-east-1 only.
+
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws.primary"></a> [aws.primary](#provider\_aws.primary) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_kms_key.primary](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key) | resource |
+| [aws_caller_identity.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_aws_account_id"></a> [aws\_account\_id](#input\_aws\_account\_id) | The AWS Account ID | `string` | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_kms_key_arn"></a> [kms\_key\_arn](#output\_kms\_key\_arn) | n/a |
+<!-- END_TF_DOCS -->
---
+++ main.tf
@@ -0,0 +1,58 @@
+data "aws_caller_identity" "current" {
+  provider = aws.primary
+}
+
+variable "aws_account_id" {
+  description = "The AWS Account ID"
+  type        = string
+  nullable    = false
+}
+
+provider "aws" {
+  alias  = "primary"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/OrganizationAccountAccessRole"
+  }
+}
+
+resource "aws_kms_key" "primary" {
+  provider                 = aws.primary
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 30
+  key_usage                = "SIGN_VERIFY"
+  multi_region             = true
+  description              = "DNSSEC KMS Key"
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+          "kms:Verify",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Resource = "*"
+        Sid      = "Allow Route 53 DNSSEC Service",
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+output "kms_key_arn" {
+  value = aws_kms_key.primary.arn
+}
